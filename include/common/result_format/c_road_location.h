6b46f1e9058a2b452f8c033008ffff61ff17a345
romanodanilo <62891297+romanodanilo@users.noreply.github.com>
2024-05-17 15:00:54 +0200

    Refactor file names to snake case format (#47)
    Signed-off-by: romanodanilo <62891297+romanodanilo@users.noreply.github.com>

---
+++ include/common/result_format/c_road_location.h
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2023 CARIAD SE.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef cRoadLocation_h__
+#define cRoadLocation_h__
+
+#include "c_extended_information.h"
+#include "../xml/util_xerces.h"
+
+/*
+* Definition of additional road location. This can be used to debug special positions
+* in dbqa framework.
+*/
+class cRoadLocation : public cExtendedInformation
+{
+
+public:
+	static const XMLCh* TAG_NAME;
+	static const XMLCh* ATTR_ROAD_ID;
+	static const XMLCh* ATTR_S;
+	static const XMLCh* ATTR_T;
+
+	/*
+	* Creates a new instance of cRoadLocation
+	* \param roadId: ID of the road in OpenDrive
+	* \param s: S of the road in OpenDrive
+	* \param t: T of the road in OpenDrive
+	*/
+	cRoadLocation(const std::string& roadId, float s = 0.0f, float t = 0.0f) : cExtendedInformation("RoadLocation")
+	{
+		m_RoadID = roadId;
+		m_S = s;
+		m_T = t;
+	}
+
+	// Serialize this information
+	virtual XERCES_CPP_NAMESPACE::DOMElement* WriteXML(XERCES_CPP_NAMESPACE::DOMDocument* p_resultDocument);
+
+	// Unserialize this information
+	static cRoadLocation* ParseFromXML(XERCES_CPP_NAMESPACE::DOMNode* pXMLNode, XERCES_CPP_NAMESPACE::DOMElement* pXMLElement);
+
+	// Returns the xPath
+    std::string GetRoadID() const;
+
+	// Returns the road Id as integer
+	void GetRoadID(int& roadId) const;
+
+	// Returns the s of the road
+	float GetS() const;
+
+	// Returns the t of the road
+	float GetT() const;
+protected:
+    std::string m_RoadID;
+	float m_S;
+	float m_T;
+
+private:
+	cRoadLocation();
+	cRoadLocation(const cRoadLocation&);
+};
+
+#endif
---
+++ include/common/xsd/c_osc_schema_checker.h
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2023 CARIAD SE.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef cOSCSchemaChecker_h__
+#define cOSCSchemaChecker_h__
+
+#include "c_parser_error_handler.h"
+#include "common/qc4openx_filesystem.h"
+#include "common/result_format/c_checker.h"
+#include "common/result_format/c_checker_bundle.h"
+#include "common/result_format/c_file_location.h"
+#include "common/result_format/c_locations_container.h"
+#include "common/result_format/c_result_container.h"
+#include "common/util.h"
+
+#include <iostream>
+#include <list>
+#include <map>
+#include <sstream>
+
+class OSCSchemaChecker {
+private:
+    cResultContainer* pResultContainer;
+    cCheckerBundle* pXSDCheckerBundle;
+    std::string checkerBundleName;
+    cParameterContainer inputParams;
+    std::string xoscFilePath;
+    std::map<std::string, std::list<fs::path>> versionToXsdFile;
+    std::list<std::string> unsupportedVersions;
+
+public:
+    OSCSchemaChecker()
+    {
+    }
+    OSCSchemaChecker(const std::string& name,
+                     cParameterContainer params,
+                     std::map<std::string, std::list<fs::path>> versionMap)
+        : checkerBundleName(name), inputParams(params), versionToXsdFile(versionMap)
+    {
+        Init();
+    }
+    ~OSCSchemaChecker()
+    {
+    }
+    void Init();
+    bool RunChecks();
+    bool ValidateXSD(const std::list<fs::path>& lSchemaFiles);
+    bool ExtractXOSCVersion(unsigned* i_uRevMajor, unsigned* i_uRevMinor);
+    void WriteEmptyReport();
+};
+
+#endif
