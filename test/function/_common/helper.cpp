d95c8680ae258b696a711a80f4af4b42b4164c8a
romanodanilo <62891297+romanodanilo@users.noreply.github.com>
2024-05-22 17:26:01 +0200

    Remove filesystem library from dev essential (#51)
    Signed-off-by: romanodanilo <danilo@ivex.ai>
    Signed-off-by: romanodanilo <62891297+romanodanilo@users.noreply.github.com>

--- test/function/_common/helper.cpp
+++ test/function/_common/helper.cpp
@@ -18,7 +18,7 @@ qc4openx::Result ExecuteCommand(std::string &strResultMessage, std::string strCo
 #endif
     strTotalCommand.append(strCommand.c_str());
 
-    if (a_util::filesystem::exists(strTotalCommand))
+    if (fs::exists(strTotalCommand))
     {
         if (!strArgument.empty())
         {
@@ -47,15 +47,31 @@ qc4openx::Result ExecuteCommand(std::string &strResultMessage, std::string strCo
 
 qc4openx::Result CheckFileExists(std::string &strResultMessage, const std::string strFilePath, const bool bDelete)
 {
-    if (a_util::filesystem::exists(strFilePath.c_str()))
+    if (fs::exists(strFilePath.c_str()))
     {
-        std::string strFileContent;
-        a_util::filesystem::readTextFile(strFilePath.c_str(), strFileContent);
+        // Open the file in input mode
+        std::ifstream file(strFilePath);
+
+        // Check if the file was successfully opened
+        if (!file.is_open())
+        {
+            std::cerr << "Failed to open the file: " << strFilePath << std::endl;
+            return 1;
+        }
+
+        // Read the entire file content into a string
+        std::ostringstream oss;
+        oss << file.rdbuf();
+        std::string strFileContent = oss.str();
+
+        // Close the file
+        file.close();
+
         if (strFileContent.size() > 0)
         {
             if (bDelete == true)
             {
-                a_util::filesystem::remove(strFilePath.c_str());
+                fs::remove(strFilePath.c_str());
             }
             return qc4openx::ERR_NOERROR;
         }
