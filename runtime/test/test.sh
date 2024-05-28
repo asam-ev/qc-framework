c56058e8f64cc1e8616a4ede656e732589163e11
romanodanilo <danilo@ivex.ai>
2024-05-28 17:35:02 +0200

    Add first tests and move DemoCheckerBundle exe
    Signed-off-by: romanodanilo <danilo@ivex.ai>

---
+++ runtime/test/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Copyright 2024, IVEX NV
+# All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+python3 -m pytest
