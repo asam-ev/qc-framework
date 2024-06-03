7bded1288026b1c546eddf9d5bde16dc0f2cd862
romanodanilo <danilo@ivex.ai>
2024-06-03 14:57:45 +0200

    Add framework_builder as base image of unit test container
    Signed-off-by: romanodanilo <danilo@ivex.ai>

---
+++ docker/test_unittest.sh
@@ -0,0 +1,15 @@
+# Copyright 2024, ASAM e.V.
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+cd ..
+
+DOCKER_BUILDKIT=1 \
+  docker build \
+  -f docker/Dockerfile.linux \
+  --target unit_test \
+  -t unit_test .
+
+docker run --rm --name unit_test unit_test
+
