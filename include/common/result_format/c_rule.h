0e7db35833e75f1a67334f4025199c91f7dc7ef4
romanodanilo <danilo@ivex.ai>
2024-06-06 14:17:53 +0200

    Add rule and metadata unit test and fix copyright notice
    Signed-off-by: romanodanilo <danilo@ivex.ai>

--- include/common/result_format/c_rule.h
+++ include/common/result_format/c_rule.h
@@ -1,10 +1,11 @@
 /**
- * Copyright 2023 CARIAD SE.
- *
+ * Copyright 2024, ASAM e.V.
+ * 
  * This Source Code Form is subject to the terms of the Mozilla
  * Public License, v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
 
 #ifndef cRule_h__
 #define cRule_h__
