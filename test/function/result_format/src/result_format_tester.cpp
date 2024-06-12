802eda518c3a77659a9aafc42d69e22ada2f7a92
romanodanilo <danilo@ivex.ai>
2024-06-12 12:40:19 +0200

    Add domain specific info test
    Signed-off-by: romanodanilo <danilo@ivex.ai>

---
+++ test/function/result_format/src/result_format_tester.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 CARIAD SE.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "gtest/gtest.h"
+
+#include "common/result_format/c_checker_bundle.h"
+#include "common/result_format/c_domain_specific_info.h"
+#include "common/result_format/c_issue.h"
+#include "common/result_format/c_result_container.h"
+#include "helper.h"
+#include <xercesc/util/PlatformUtils.hpp>
+
+#define MODULE_NAME "ResultFormat"
+
+class cTesterResultFormat : public ::testing::Test
+{
+  public:
+    std::string strTestFilesDir = std::string(QC4OPENX_DBQA_RESULT_FORMAT_TEST_REF_DIR);
+    std::string strWorkingDir = std::string(QC4OPENX_DBQA_RESULT_FORMAT_TEST_WORK_DIR);
+};
+
+TEST_F(cTesterResultFormat, DomainSpecificInfoReadWrite)
+{
+    XERCES_CPP_NAMESPACE::XMLPlatformUtils::Initialize();
+    std::string strResultMessage;
+    std::string strFilePath = strTestFilesDir + "/result_domain_info.xqar";
+    std::string strResultFile = strWorkingDir + "/output.xqar";
+    std::string strXsdFilePath = strTestFilesDir + "/../../doc/schema/xqar_report_format.xsd";
+    // Check if xsd file exists
+    TestResult nRes = CheckFileExists(strResultMessage, strXsdFilePath, false);
+    ASSERT_TRUE_EXT(nRes == TestResult::ERR_NOERROR, strResultMessage.c_str());
+
+    cResultContainer *pResultContainer = new cResultContainer();
+    // Parse results from xml
+    pResultContainer->AddResultsFromXML(strFilePath);
+
+    // Check domain specifc if is parsed
+    int domain_specific_info_count = 0;
+    std::list<cCheckerBundle *> checkerBundles = pResultContainer->GetCheckerBundles();
+    for (std::list<cCheckerBundle *>::const_iterator itCheckerBundle = checkerBundles.cbegin();
+         itCheckerBundle != checkerBundles.cend(); itCheckerBundle++)
+    {
+        std::list<cIssue *> issues = (*itCheckerBundle)->GetIssues();
+        for (std::list<cIssue *>::const_iterator itIssue = issues.cbegin(); itIssue != issues.cend(); itIssue++)
+        {
+            domain_specific_info_count += (*itIssue)->GetDomainSpecificCount();
+        }
+    }
+
+    std::cout << "Domain specific info count : " << domain_specific_info_count << std::endl;
+    ASSERT_TRUE_EXT(domain_specific_info_count > 0, "No domain specific info found");
+
+    // Write results to XML
+    pResultContainer->WriteResults(strResultFile);
+
+    // Check if output file exists
+    nRes |= CheckFileExists(strResultMessage, strResultFile, false);
+    ASSERT_TRUE_EXT(nRes == TestResult::ERR_NOERROR, strResultMessage.c_str());
+    // Check if result file is valid according to xsd
+    nRes |= ValidateXmlSchema(strResultFile, strXsdFilePath);
+    ASSERT_TRUE_EXT(nRes == TestResult::ERR_NOERROR, strResultMessage.c_str());
+
+    // Check if result file contains a specific tag
+    nRes |= XmlContainsNode(strFilePath, "RoadLocation");
+    ASSERT_TRUE_EXT(nRes == TestResult::ERR_NOERROR, strResultMessage.c_str());
+
+    delete pResultContainer;
+    XERCES_CPP_NAMESPACE::XMLPlatformUtils::Terminate();
+}
