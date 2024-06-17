06cfad0a94fe99d99e3736c067a981d8793b8efe
romanodanilo <62891297+romanodanilo@users.noreply.github.com>
2024-06-17 18:04:59 +0200

    Add demo pipeline (#82)
    Signed-off-by: romanodanilo <danilo@ivex.ai>
    
    Signed-off-by: romanodanilo <62891297+romanodanilo@users.noreply.github.com>

---
+++ demo_pipeline/README.md
@@ -0,0 +1,58 @@
+# Demo pipeline
+
+The demo pipeline allow users to process OpenDRIVE and OpenSCENARIO files with respecting checkers and inspect the resulting `xqar` and `txt` files
+
+### Download and run
+
+The demo pipeline is provided as docker image uploaded to Github container registry
+
+To process a file it is needed to mount input and output folder and provide the filename to process
+
+```
+docker run \
+    -v YOUR_INPUT_FOLDER:/input_directory \
+    -v YOUR_OUTPUT_FOLDER:/out \
+    -e INPUT_FILENAME=YOUR_INPUT_FILENAME \
+    --rm --name demo_pipeline ghcr.io/asam-ev/qc-framework:demo-pipeline-latest
+```
+
+E.g. To process the file at `/home/user/xodr_files/test_ramp.xosc`
+
+```
+docker run \
+    -v /home/user/xodr_files:/input_directory \
+    -v /home/user/output:/out \
+    -e INPUT_FILENAME=test_ramp.xosc \
+    --rm --name demo_pipeline ghcr.io/asam-ev/qc-framework:demo-pipeline-latest
+```
+
+
+The docker image will automatically:
+- Detect the type of file passed as input
+- Create the specific config according to [config schema](../doc/schema/config_format.xsd)
+- Execute the runtime with specific checker, result pooling and text result application
+
+Currently the demo_pipeline will clone and execute:
+
+- [OpenDRIVE checker @ develop branch](https://github.com/asam-ev/qc-opendrive/tree/develop)
+- [OpenSCENARIO checker @ develop branch](https://github.com/asam-ev/qc-openscenarioxml/tree/develop)
+
+After the execution, in the specified output folder you will find:
+
+- Specific CheckerBundle `xqar` result file
+- ResultPooling `Result.xqar` result file
+- TextReport `Report.txt` text file
+
+### Build instructions
+
+In case of local build of demo_pipeline docker image, you can execute:
+
+```
+cd ..
+
+DOCKER_BUILDKIT=1 \
+  docker build \
+  -f docker/Dockerfile.linux \
+  --target demo_pipeline \
+  -t demo_pipeline .
+```
---
+++ demo_pipeline/configuration_generator.py
@@ -0,0 +1,64 @@
+# Copyright 2024, ASAM e.V.
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import sys, os, glob
+from lxml import etree
+
+XODR_TEMPLATE_PATH = "/app/demo_pipeline/templates/xodr_template.xml"
+XOSC_TEMPLATE_PATH = "/app/demo_pipeline/templates/xosc_template.xml"
+GENERATED_CONFIG_PATH = "/tmp/generated_config"
+
+
+def update_param_value(xml_file, name, new_value, output_file):
+    # Parse the XML file
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+
+    # Find the Param element with the name attribute and update its value attribute
+    for param in root.findall(f".//Param[@name='{name}']"):
+        param.set("value", new_value)
+
+    # Write the updated XML to the output file
+    tree.write(output_file, encoding="utf-8", pretty_print=True, xml_declaration=True)
+
+
+def main():
+    input_directory = "/input_directory"
+    input_filename = os.getenv("INPUT_FILENAME")
+
+    full_input_path = os.path.join(input_directory, input_filename)
+
+    os.makedirs(GENERATED_CONFIG_PATH, exist_ok=True)
+
+    if not os.path.isfile(full_input_path):
+        print("No file specified as input. Please provide xosc or xodr file. Exiting...")
+        return
+
+    print("Input file: ", full_input_path)
+    _, input_file_extension = os.path.splitext(full_input_path)
+
+    if input_file_extension == ".xosc":
+        print("XOSC selected")
+        update_param_value(
+            XOSC_TEMPLATE_PATH,
+            "XoscFile",
+            full_input_path,
+            os.path.join(GENERATED_CONFIG_PATH, "config.xml"),
+        )
+    elif input_file_extension == ".xodr":
+        print("XODR selected")
+        update_param_value(
+            XODR_TEMPLATE_PATH,
+            "XodrFile",
+            full_input_path,
+            os.path.join(GENERATED_CONFIG_PATH, "config.xml"),
+        )
+    else:
+        print(f"Error in input file extension. Unrecognized {input_file_extension}")
+        return
+
+
+if __name__ == "__main__":
+    main()
