11034b6d0d48c0b882133e220e36619b899d488b
romanodanilo <danilo@ivex.ai>
2024-06-24 12:40:58 +0200

    Add config option to result pooling
    Signed-off-by: romanodanilo <danilo@ivex.ai>

--- src/result_pooling/src/result_pooling.cpp
+++ src/result_pooling/src/result_pooling.cpp
@@ -7,6 +7,8 @@
  */
 #include "result_pooling.h"
 #include "common/config_format/c_configuration.h"
+#include "common/config_format/c_configuration_checker.h"
+#include "common/config_format/c_configuration_checker_bundle.h"
 #include "common/result_format/c_checker_bundle.h"
 #include "common/result_format/c_file_location.h"
 #include "common/result_format/c_inertial_location.h"
@@ -20,18 +22,99 @@
 
 cResultContainer *pResultContainer;
 
+bool isXmlFile(const std::string &filename)
+{
+    const std::string extension = ".xml";
+    if (filename.size() >= extension.size() &&
+        filename.compare(filename.size() - extension.size(), extension.size(), extension) == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isDirectory(const std::string &path)
+{
+    return fs::is_directory(path);
+}
+
 // Main Programm
 int main(int argc, char *argv[])
 {
-    std::string strToolpath = argv[0];
+    XMLPlatformUtils::Initialize();
+    std::vector<std::string> args(argv, argv + argc);
+    std::string strToolpath = args[0];
 
-    if (argc > 2)
+    bool config_file_set = false;
+    bool result_dir_set = false;
+    std::string config_file;
+    std::string result_dir;
+
+    if (args.size() == 1)
     {
-        ShowHelp(strToolpath);
-        return -1;
+        // No arguments provided other than the program name
+        std::cout << "No arguments provided.\n";
     }
+    else if (args.size() == 2)
+    {
+        // One argument provided
+        std::string arg = args[1];
+        if (arg == "-h" || arg == "--help")
+        {
+            ShowHelp(strToolpath);
+            return 0;
+        }
+        else if (isXmlFile(arg))
+        {
+            config_file = arg;
+            config_file_set = true;
+        }
+        else if (isDirectory(arg))
+        {
+            result_dir = arg;
+            result_dir_set = true;
+        }
+        else
+        {
+            std::cerr << "Invalid argument: " << arg << "\n";
+            return 1; // Return error code
+        }
+    }
+    else if (args.size() == 3)
+    {
+        // Two arguments provided
+        std::string arg1 = args[1];
+        std::string arg2 = args[2];
 
-    QCoreApplication app(argc, argv);
+        if (isXmlFile(arg1) && isDirectory(arg2))
+        {
+            std::cout << "Configuration file: " << arg1 << "\n";
+            std::cout << "Results directory: " << arg2 << "\n";
+            config_file = arg1;
+            result_dir = arg2;
+            config_file_set = true;
+            result_dir_set = true;
+        }
+        else if (isDirectory(arg1) && isXmlFile(arg2))
+        {
+            std::cout << "Results directory: " << arg1 << "\n";
+            std::cout << "Configuration file: " << arg2 << "\n";
+            result_dir = arg1;
+            config_file = arg2;
+            config_file_set = true;
+            result_dir_set = true;
+        }
+        else
+        {
+            std::cerr << "Invalid arguments: " << arg1 << " and " << arg2 << "\n";
+            return 1; // Return error code
+        }
+    }
+    else
+    {
+        std::cerr << "Invalid number of arguments.\n";
+        return 1; // Return error code
+    }
 
     cParameterContainer inputParams;
 
@@ -39,39 +122,40 @@ int main(int argc, char *argv[])
     inputParams.SetParam("strResultFile", "Result.xqar");
     fs::path resultsDirectory = GetWorkingDir();
 
-    if (argc == 2) // else: use default parameter
+    // If specified, use second argument to specify result directory, else: use default parameter
+    if (result_dir_set)
     {
-        std::string strFilepath = argv[1];
-
-        if (strcmp(strFilepath.c_str(), "-h") == 0 || strcmp(strFilepath.c_str(), "--help") == 0)
-        {
-            ShowHelp(strToolpath);
-            return 0;
-        }
+        // Compute path to result files
+        fs::path pFilepath = result_dir;
+        if (pFilepath.is_absolute())
+            resultsDirectory = pFilepath;
         else
         {
-            // Compute path to result files
-            fs::path pFilepath = strFilepath;
-            if (pFilepath.is_absolute())
-                resultsDirectory = pFilepath;
+            if ("/" == result_dir)
+                resultsDirectory += result_dir;
             else
-            {
-                if ("/" == strFilepath)
-                    resultsDirectory += strFilepath;
-                else
-                    resultsDirectory = resultsDirectory.append(strFilepath);
-            }
-
-            if (!exists(resultsDirectory))
-            {
-                std::cerr << "Directory '" << resultsDirectory << "' does not exist!" << std::endl
-                          << "Abort generating report!" << std::endl;
-                return -1;
-            }
+                resultsDirectory = resultsDirectory.append(result_dir);
+        }
+        std::cout << "resultsDirectory: " << resultsDirectory << std::endl;
+        if (!exists(resultsDirectory))
+        {
+            std::cerr << "Directory '" << resultsDirectory << "' does not exist!" << std::endl
+                      << "Abort generating report!" << std::endl;
+            return -1;
         }
     }
 
-    RunResultPooling(inputParams, resultsDirectory);
+    if (config_file_set)
+    {
+        RunResultPoolingWithConfig(inputParams, resultsDirectory, config_file);
+    }
+    else
+    {
+        RunResultPooling(inputParams, resultsDirectory);
+    }
+
+    XMLPlatformUtils::Terminate();
+
     return 0;
 }
 
@@ -83,16 +167,15 @@ void ShowHelp(const std::string &toolPath)
     GetFileName(&applicationNameWithoutExt, true);
 
     std::cout << "\n\nUsage of " << applicationNameWithoutExt << ":" << std::endl;
-    std::cout << "\nRun the application to summarize all xqar files from current directory: \n"
-              << applicationName << std::endl;
-    std::cout << "\nRun the application to summarize all xqar files from a specified directory: \n"
-              << applicationName << " ../results/" << std::endl;
+    std::cout << "\nRun the application to summarize all xqar files from current directory with given config: \n"
+              << applicationName << " config.xml " << std::endl;
+    std::cout << "\nRun the application to summarize all xqar files from a specified directory with given config: \n"
+              << applicationName << " config.xml ../results/" << std::endl;
     std::cout << "\n\n";
 }
 
 void RunResultPooling(const cParameterContainer &inputParams, const fs::path &resultsDirectory)
 {
-    XMLPlatformUtils::Initialize();
     std::string strResultFile = inputParams.GetParam("strResultFile");
 
     pResultContainer = new cResultContainer();
@@ -140,7 +223,85 @@ void RunResultPooling(const cParameterContainer &inputParams, const fs::path &re
     std::cout << "Finished." << std::endl;
 
     delete pResultContainer;
-    XMLPlatformUtils::Terminate();
+}
+
+void RunResultPoolingWithConfig(cParameterContainer &inputParams, const fs::path &resultsDirectory,
+                                const std::string &configFile)
+{
+    cConfiguration configuration;
+
+    std::cout << "Config: " << configFile << std::endl;
+    if (!cConfiguration::ParseFromXML(&configuration, configFile))
+    {
+        std::cerr << "Could not read configuration! Abort." << std::endl;
+        return;
+    }
+    inputParams.Overwrite(configuration.GetParams());
+
+    std::string strResultFile = inputParams.GetParam("strResultFile");
+
+    pResultContainer = new cResultContainer();
+
+    std::cout << std::endl;
+
+    // Delete old result file in working directory
+    std::cout << "Cleanup: ";
+    if (fs::exists(strResultFile))
+    {
+        std::cout << "Delete: " << std::endl;
+        std::cout << ">  " << strResultFile;
+
+        QFile file(strResultFile.c_str());
+        file.remove();
+    }
+    std::cout << std::endl << std::endl;
+    std::cout << "Collect results from directory: " << std::endl
+              << resultsDirectory << std::endl
+              << "According to config file: " << std::endl
+              << configFile << std::endl;
+
+    std::vector<cConfigurationCheckerBundle *> checkerBundleConfigs = configuration.GetCheckerBundles();
+    for (std::vector<cConfigurationCheckerBundle *>::const_iterator itCheckerBundles = checkerBundleConfigs.cbegin();
+         itCheckerBundles != checkerBundleConfigs.end(); itCheckerBundles++)
+    {
+        std::cout << "----> result file " << (*itCheckerBundles)->GetParam("strResultFile") << std::endl;
+        std::vector<cConfigurationChecker *> checkers = (*itCheckerBundles)->GetCheckers();
+
+        for (std::vector<cConfigurationChecker *>::const_iterator itChecker = checkers.cbegin();
+             itChecker != checkers.end(); itChecker++)
+        {
+            std::cout << "----> checker id " << (*itChecker)->GetCheckerId() << std::endl;
+        }
+    }
+
+    std::cout << "Found: " << std::endl;
+    for (auto &pFilePath : fs::directory_iterator(resultsDirectory))
+    {
+        std::string strFilePath = pFilePath.path().string();
+        std::string strFileName = strFilePath;
+        GetFileName(&strFileName, false);
+
+        // Check if we have an result file in the resultsDirectory
+        if (ToLower(strFileName) == ToLower(strResultFile))
+            continue;
+
+        if (StringEndsWith(strFileName, "xqar"))
+        {
+            std::cout << ">  " << strFileName << "\t\tReading..." << std::endl;
+            pResultContainer->AddResultsFromXML(strFilePath);
+        }
+    }
+
+    std::cout << std::endl << "Find locations in xml file..." << std::endl << std::endl;
+
+    AddFileLocationsToIssues();
+
+    std::cout << "Write report: '" << strResultFile << "'" << std::endl << std::endl;
+    pResultContainer->WriteResults(strResultFile);
+
+    std::cout << "Finished." << std::endl;
+
+    delete pResultContainer;
 }
 
 static void AddFileLocationsToIssues()
