708b0b9bf648537cd4e407878481673a9448e211
Pierre R. Mai <pmai@pmsf.de>
2025-11-18 21:33:01 +0100

    Initial command viewer implementation
    Signed-off-by: Pierre R. Mai <pmai@pmsf.de>

---
+++ doc/manual/command_viewer_plugin.md
@@ -0,0 +1,67 @@
+# Command viewer plugin
+
+The command viewer plugin is a plugin to open the external applications based on the input file format and use calls to external commands to visualize an input file.
+For each issue that contains supported location information (as matched via an XPath expression), the viewer will call an external command - passing information using a command template - to highlight the issue in the external viewer.
+
+## Installation
+
+After building the framework, a new plugin will be shown in the `File` dropdown menu of the ReportGUI application.
+
+It is started automatically during startup and will support any configured formats (see below).
+To restart the viewer plugin, the `File` dropdown menu of the ReportGUI application can be used.
+
+![command_viewer](images/command_viewer.png)
+
+Before using the plugin, relevant configuration needs to occur in the platform native way for Qt applications (see below).
+
+## Configuration
+
+The behavior of the command viewer plugin is controlled using the native Qt mechanisms for settings (`QSettings`).
+This means that for Windows the settings are stored in the registry, for Linux in `conf` files, and for macOS in `plist` files.
+
+The settings are user-settings under the `ASAM` organization and `QCReportGUICommandViewer` application name:
+
+![command_viewer_configuration](images/command_viewer_config.png)
+
+The main setting is the `formats` array, that will contain an entry for each file format to be supported by the command viewer.
+
+Each entry in format contains the following set of keys:
+
+- `format` (mandatory): The file extension (including any leading dot), that the input file shall have to be supported (example: `.osi`)
+- `name` (mandatory): The format name as a user-readable string (example: `OSI Single Trace File`).
+- `start_executable` (optional): A path to an executable that is to be executed once the viewer is started and initialized with an input file.
+- `start_template` (optional): Template of the command line to execute once the viewer is started and initialized with an input file.
+- `show_location_xpath` (optional): XPath to check for a showable location in an issue, and extract any necessary additional information for the `show_template`.
+- `show_executable` (optional): A path to an executable that is to be executed once the viewer is instructed to show an issue and its location.
+- `show_template` (optional): Template of the command line to execute when the viewer is instructed to show an issue and its location.
+- `stop_executable` (optional): A path to an executable that is to be executed once the viewer is closing down.
+- `stop_template` (optional): Template of the command line to execute once the viewer is closing down.
+
+The way execution works for the individual steps follows the following rules:
+
+If a `*_template` setting is found, then it is expanded into a full command-line using pattern substitution:
+
+- Any occurrence of `%1` is replaced by the corresponding `*_executable` value.
+- Any occurrence of `%2` is replaced by the input file path.
+- For the `show_template`, any string result of the `show_location_xpath` is accessible with `%3` and so on.
+- Double quotes can be used to guard against spaces for the splitting into command line arguments prior to execution.
+
+Any executed commands are executed in parallel and should terminate on their own.
+The plugin does not wait for termination, only for proper startup of the executed commands.
+
+### Example Configuration for OSI and Lichtblick
+
+As an example of the use of To support the visualization using Lichtblick of issues in OSI multi-channel trace files, as detected by the qc-osi-trace checker bundle, the following configuration is suitable:
+
+- `format=.mcap`
+- `name=OSI Multi Trace File`
+- `show_location_xpath="//MessageLocation/(@time/string(),@channel/string())"`
+- `show_executable=c:\\Users\\username\\AppData\\Local\\Programs\\lichtblick\\Lichtblick.exe`
+- `show_template="%1 --time=%3 \"%2\""`
+
+This means that for `.mcap` input files the command viewer will make all issues that have a `MessageLocation` location with `time` and `channel` attributes accessible.
+It will execute the `Lichtblick` executable with the message time as its `--time` argument and the input file (`%2`) as a non-option argument.
+
+If no current configuration for the command viewer plugin is detected on startup, a default configuration for Lichtblick is added.
+
+![command_viewer_lichtblick](images/command_viewer_lichtblick.png)
---
+++ examples/command_viewer/command_viewer.cpp
@@ -0,0 +1,323 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ * Copyright 2023 CARIAD SE.
+ * Copyright 2024 ASAM e.V.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "common/result_format/c_issue.h"
+#include "common/result_format/c_locations_container.h"
+#include "viewer/i_connector.h"
+#include <cstring>
+#include <stdio.h>
+#include <QtCore/QString>
+#include <QtCore/QSettings>
+#include <QtCore/QProcess>
+#include <QtXml/QDomDocument>
+#include <QtXmlPatterns/QXmlQuery>
+
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/dom/DOMImplementation.hpp>
+#include <xercesc/dom/DOMImplementationLS.hpp>
+#include <xercesc/dom/DOMLSSerializer.hpp>
+#include <xercesc/dom/DOMLSOutput.hpp>
+#include <xercesc/framework/MemBufFormatTarget.hpp>
+
+XERCES_CPP_NAMESPACE_USE
+
+const char *lasterrormsg = "";
+QString current_input_path = "";
+QString current_format = "";
+QString current_format_name = "";
+QString current_start_executable = "";
+QString current_start_template = "";
+QString current_show_location_xpath = "";
+QString current_show_executable = "";
+QString current_show_template = "";
+QString current_stop_executable = "";
+QString current_stop_template = "";
+
+QSettings GetSettings()
+{
+#ifdef USE_INI_SETTINGS
+    return QSettings(QSettings::IniFormat, QSettings::UserScope, "ASAM", "QCReportGUICommandViewer");
+#else
+    return QSettings("ASAM", "QCReportGUICommandViewer");
+#endif
+}
+
+void MaybeInitializeDefaultSettings()
+{
+    QSettings settings = GetSettings();
+
+    // Maybe initialize default settings
+    if (!settings.contains("formats/size"))
+    {
+        settings.beginWriteArray("formats");
+        settings.setArrayIndex(0);
+        settings.setValue("format", ".xodr");
+        settings.setValue("name", "OpenDRIVE");
+        settings.setValue("start_executable", "opendrive_viewer");
+        settings.setValue("start_template", "%1 %2");
+        settings.setValue("show_executable", "opendrive_viewer");
+        settings.setValue("show_template", "%1 --at %3:%4 %2");
+        settings.setValue("show_location_xpath", "//FileLocation/(@row/string(),@column/string())");
+        settings.setValue("stop_executable", "opendrive_viewer");
+        settings.setValue("stop_template", "%1 --close %2");
+        settings.setArrayIndex(1);
+        settings.setValue("format", ".osi");
+        settings.setValue("name", "OSI Single Trace File");
+        settings.setValue("show_executable", "osiviewer");
+        settings.setValue("show_location_xpath", "//MessageLocation/(@time/string(),@index/string())");
+        settings.setValue("show_template", "%1 --file %2 --time %3 --index %4");
+        settings.setArrayIndex(2);
+        settings.setValue("format", ".mcap");
+        settings.setValue("name", "OSI Multi Trace File");
+        settings.setValue("show_executable", "c:\\Users\\pmai\\AppData\\Local\\Programs\\lichtblick\\Lichtblick.exe");
+        settings.setValue("show_location_xpath", "//MessageLocation/(@time/string(),@channel/string())");
+        settings.setValue("show_template", "%1 --time=%3 \"%2\"");
+        settings.endArray();
+    }
+}
+
+bool CanSupportFormat(const char *inputPath)
+{
+    if (inputPath == nullptr || std::strcmp(inputPath, "") == 0)
+    {
+        lasterrormsg = "ERROR: No valid input file found.";
+        return false;
+    }
+
+    MaybeInitializeDefaultSettings();
+
+    std::cout << "CAN COMMAND VIEWER SUPPORT FORMAT WITH INPUT FILE: " << inputPath << "? ";
+    QString inputPathStr = QString(inputPath);
+    QSettings settings = GetSettings();
+    int size = settings.beginReadArray("formats");
+    for (int i = 0; i < size; ++i)
+    {
+        settings.setArrayIndex(i);
+        QString format = settings.value("format").toString();
+        QString name = settings.value("name").toString();
+        if (inputPathStr.endsWith(format))
+        {
+            settings.endArray();
+            std::cout << "YES - " << name.toStdString() << " (" << format.toStdString() << ")" << std::endl;
+            return true;
+        }
+    }
+    settings.endArray();
+    std::cout << "NO" << std::endl;
+
+    return false;
+}
+
+bool StartViewer()
+{
+    std::cout << "START COMMAND VIEWER" << std::endl;
+    return true;
+}
+
+bool Initialize(const char *inputPath)
+{
+    if (inputPath == nullptr || std::strcmp(inputPath, "") == 0)
+    {
+        lasterrormsg = "ERROR: No valid input file found.";
+        return false;
+    }
+    std::cout << "INITILAIZE COMMAND VIEWER WITH INPUT FILE: " << inputPath << std::endl;
+
+    current_input_path = QString(inputPath);
+
+    QSettings settings = GetSettings();
+    int size = settings.beginReadArray("formats");
+    for (int i = 0; i < size; ++i)
+    {
+        settings.setArrayIndex(i);
+        QString format = settings.value("format").toString();
+        if (current_input_path.endsWith(format))
+        {
+            current_format = format;
+            current_format_name = settings.value("name").toString();
+            current_start_executable = settings.value("start_executable").toString();
+            current_start_template = settings.value("start_template").toString();
+            current_show_location_xpath = settings.value("show_location_xpath").toString();
+            current_show_executable = settings.value("show_executable").toString();
+            current_show_template = settings.value("show_template").toString();
+            current_stop_executable = settings.value("stop_executable").toString();
+            current_stop_template = settings.value("stop_template").toString();
+            settings.endArray();
+            std::cout << "Format " << current_format_name.toStdString() << " (" << current_format.toStdString() << ") - Start Executable " << current_start_executable.toStdString() << std::endl;
+
+            if (!current_start_template.isEmpty())
+            {
+                QString command = current_start_template.arg(current_stop_executable, current_input_path);
+                std::cout << "EXECUTING: " << command.toStdString() << std::endl;
+
+                QProcess process;
+                QStringList args = QProcess::splitCommand(command);
+                process.startDetached(args.takeFirst(), args);
+                process.waitForStarted(-1); // will wait forever until started
+            }
+
+            return true;
+        }
+    }
+    settings.endArray();
+
+    lasterrormsg = "ERROR: No valid format found for input file.";
+    return false;
+}
+
+bool AddIssue(void *issueToAdd)
+{
+    auto issue = static_cast<cIssue *>(issueToAdd);
+    std::cout << "COMMAND VIEWER ADD ISSUE: " << issue->GetDescription() << std::endl;
+    return true;
+}
+
+bool MatchXML(cLocationsContainer *location, const QString &expression, QStringList &results)
+{
+    // Build a Xerces DOMDocument, ask the location to populate it, serialize to QString,
+    // then load into a QDomDocument.
+    XMLCh *lsId = XMLString::transcode("LS");
+    DOMImplementation *impl = DOMImplementationRegistry::getDOMImplementation(lsId);
+    XMLString::release(&lsId);
+    if (!impl) {
+        lasterrormsg = "ERROR: No DOMImplementation with LS support.";
+        return false;
+    }
+
+    DOMDocument *xercesDoc = impl->createDocument();
+    if (!xercesDoc) {
+        lasterrormsg = "ERROR: Failed to create Xerces DOMDocument.";
+        return false;
+    }
+
+    // Let the location populate the Xerces DOM. Signature assumed: void WriteXML(DOMDocument*)
+    DOMElement *pSummary = location->WriteXML(xercesDoc);
+    xercesDoc->appendChild(pSummary);
+
+    // Serialize Xerces DOM to a UTF-8 string
+    DOMImplementationLS *implLS = dynamic_cast<DOMImplementationLS *>(impl);
+    if (!implLS) {
+        lasterrormsg = "ERROR: DOMImplementationLS not available.";
+        xercesDoc->release();
+        return false;
+    }
+
+    DOMLSSerializer *serializer = implLS->createLSSerializer();
+    if (!serializer) {
+        lasterrormsg = "ERROR: Failed to create serializer.";
+        xercesDoc->release();
+        return false;
+    }
+    
+    DOMLSOutput *output = implLS->createLSOutput();
+    XMLCh* utf8Encoding = XMLString::transcode("UTF-8");
+    output->setEncoding(utf8Encoding);
+    XMLString::release(&utf8Encoding);
+    MemBufFormatTarget* memTarget = new MemBufFormatTarget();
+    output->setByteStream(memTarget);
+    serializer->write(xercesDoc, output);
+
+    const XMLByte* xmlBytes = memTarget->getRawBuffer();
+    size_t size = memTarget->getLen();
+
+    QString xmlString = QString::fromUtf8(reinterpret_cast<const char*>(xmlBytes), static_cast<int>(size));
+
+    delete memTarget;
+    output->release();
+    serializer->release();
+    xercesDoc->release();
+    // Now parse the serialized XML into a QDomDocument
+    QXmlQuery query(QXmlQuery::XQuery10);
+    query.setFocus(xmlString);
+    query.setQuery(expression);
+    if (!query.evaluateTo(&results)) {
+        std::cout << "Failed to evaluate XPath expression: " << expression.toStdString() << (query.isValid()?"":" is invalid") << std::endl;
+        lasterrormsg = "ERROR: Failed to evaluate XPath expression.";
+        return false;
+    }
+#if 0
+    std::cout << "XPath Results:" << std::endl;
+    for (const QString &res : results)
+    {
+        std::cout << " - " << res.toStdString() << std::endl;
+    }
+#endif
+    return results.size() > 0;
+}
+
+bool CanShowIssue(void *itemToShow, void *locationToShow)
+{
+    if (locationToShow == nullptr)
+        return false;
+
+    cLocationsContainer *location = static_cast<cLocationsContainer *>(locationToShow);
+    QStringList results;
+    return MatchXML(location, current_show_location_xpath, results);
+}
+
+bool ShowIssue(void *itemToShow, void *locationToShow)
+{
+    auto issue = static_cast<cIssue *>(itemToShow);
+    auto location = static_cast<cLocationsContainer *>(locationToShow);
+    std::cout << "COMMAND VIEWER SHOW ISSUE: " << issue->GetDescription() << std::endl;
+    std::cout << "CV LOCATION: " << location->GetDescription() << std::endl;
+
+    QStringList results;
+    if (!MatchXML(location, current_show_location_xpath, results))
+    {
+        lasterrormsg = "ERROR: Location does not match show xpath.";
+        return false;
+    }
+
+    if (!current_show_template.isEmpty())
+    {
+        QString command = current_show_template.arg(current_show_executable, current_input_path);
+        for (const QString &res : results)
+        {
+            command = command.arg(res);
+        }
+        std::cout << "EXECUTING: " << command.toStdString() << std::endl;
+
+        QProcess process;
+        QStringList args = QProcess::splitCommand(command);
+        process.startDetached(args.takeFirst(), args);
+        process.waitForStarted(-1); // will wait forever until started
+    }
+    return true;
+}
+
+const char *GetName()
+{
+    return "Command Viewer";
+}
+
+bool CloseViewer()
+{
+    std::cout << "CLOSE COMMAND VIEWER" << std::endl;
+    if (!current_stop_template.isEmpty())
+    {
+        QString command = current_stop_template.arg(current_stop_executable, current_input_path);
+        std::cout << "EXECUTING: " << command.toStdString() << std::endl;
+
+        QProcess process;
+        QStringList args = QProcess::splitCommand(command);
+        process.startDetached(args.takeFirst(), args);
+        process.waitForStarted(-1); // will wait forever until started
+    }
+
+    return true;
+}
+
+const char *GetLastErrorMessage()
+{
+    return lasterrormsg;
+}
